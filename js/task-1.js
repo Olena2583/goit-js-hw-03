59350b70e00417fc8cc091781643daffa9656636
Olena2583 <e.sorochan@o2.pl>
2024-09-08 18:24:45 +0200

    fix goit-js-hw-03

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,4 @@
+`use strict`;
 function slugify(title) {
   return title.toLowerCase().split(' ').join('-');
 }
--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,4 @@
+`use strict`;
 function filterArray(numbers, value) {
   const Numbers = [];
   for (let i = 0; i < numbers.length; i++) {
